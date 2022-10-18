c59bb96a3f2de3c07c1df4077043a65bf11f4720
Luis Juárez <luisenriquejuarez17@gmail.com>
2022-10-18 12:22:04 -0600

    Creación y modificación de pages (f-t)

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.models';
 
 @Component({
   selector: 'app-media-player',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./media-player.component.css']
 })
 export class MediaPlayerComponent implements OnInit {
+
+  mockCover: TrackModel = {
+    cover: "https://i.scdn.co/image/ab67616d0000b27345ca41b0d2352242c7c9d4bc",
+    album: "Gioli y Assia",
+    name: "BEBE (Oficial)",
+    url: "http://localhost/track.mp3",
+    _id: 1
+  }
 
   constructor() { }
 
